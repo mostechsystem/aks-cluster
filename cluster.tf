c5c5714700af38d267d4e1dc13f2465df7fd57a6
mostechsystem <mostechsystem@gmail.com>
2025-01-05 12:58:49 +0100

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
-  kubernetes_version  = "1.27.4"
+  kubernetes_version  = "1.27.101"
 
   default_node_pool {
     name            = "default"
