0dc7a441e1c1954cbdccf8bde91a1c986b73975a
mostechsystem <mostechsystem@gmail.com>
2025-01-05 11:52:33 +0100

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -5,6 +5,10 @@ resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
+  client_id       = var.app
+  client_secret   = var.password
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "default" {
