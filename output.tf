eb9834acc26863308d41f0d69744a5d9521434dd
mostechsystem <mostechsystem@gmail.com>
2025-01-05 12:38:52 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,34 +1,39 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Output for Resource Group Name
 output "resource_group_name" {
   value = azurerm_resource_group.default.name
 }
 
+# Output for Kubernetes Cluster Name
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
 
+# Output for Kubernetes Cluster Kubeconfig Host
 output "host" {
-   value = azurerm_kubernetes_cluster.default.kube_config.0.host
+  value = azurerm_kubernetes_cluster.default.kube_config.0.host
 }
 
+# Output for Kubernetes Cluster Client Key
 output "client_key" {
-   value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
+  value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
 }
 
+# Output for Kubernetes Cluster Client Certificate
 output "client_certificate" {
-   value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
 }
 
+# Output for Raw Kubernetes Cluster Kubeconfig
 output "kube_config" {
-   value = azurerm_kubernetes_cluster.default.kube_config_raw
+  value = azurerm_kubernetes_cluster.default.kube_config_raw
 }
 
+# Output for Kubernetes Cluster Username
 output "cluster_username" {
-   value = azurerm_kubernetes_cluster.default.kube_config.0.username
+  value = azurerm_kubernetes_cluster.default.kube_config.0.username
 }
 
+# Output for Kubernetes Cluster Password
 output "cluster_password" {
-   value = azurerm_kubernetes_cluster.default.kube_config.0.password
+  value = azurerm_kubernetes_cluster.default.kube_config.0.password
 }
