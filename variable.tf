25bddc1665ca3e1a0e687c40b66fc35b55d2a39c
mostechsystem <mostechsystem@gmail.com>
2025-01-05 11:51:09 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,3 +8,14 @@ variable "appId" {
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
 }
+
+
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Azure tenant ID"
+  type        = string
+}
